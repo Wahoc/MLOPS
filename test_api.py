e6864c61a29b25b9240ceed1d58d6048f661b312
JohannKerlirzin <52910571+Wahoc@users.noreply.github.com>
2026-01-16 08:39:37 +0000

    premier comit

---
+++ test_api.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'http://127.0.0.1:5000/predict'
+
+data = {
+    "Age" : 42,
+    "Account_Manager" : 1,
+    "Years" : 3,
+    "Num_Sites" : 18
+}
+
+response = requests.post(url, data=data)
+print(response.json())
